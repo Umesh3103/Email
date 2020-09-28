159f3640bab2e2110573507ddf191232f9165309
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-28 21:42:33 +0530

    [Umesh]Added: checking for mandatory .com condition

--- src/com/learning/EmailValidation.java
+++ src/com/learning/EmailValidation.java
@@ -2,7 +2,7 @@ package com.learning;
 import java.util.regex.*;
 
 public class EmailValidation {
-	private static final Pattern PIN_CODE = Pattern.compile("^[0-9 a-z A-Z]+[@][0-9 a-z A-Z]+");
+	private static final Pattern PIN_CODE = Pattern.compile("^[0-9 a-z A-Z]+[@][0-9 a-z A-Z]+[.][a-z A-Z]{2,4}");
     private static boolean checkName(String name) {
         return PIN_CODE.matcher(name).matches();
     }
@@ -10,7 +10,7 @@ public class EmailValidation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to email validation problem");
 		
-		boolean flag =checkName("abc@gmail");
+		boolean flag =checkName("abc@gmail.com");
 		if(flag){
 			System.out.println("Valid email");
 		}

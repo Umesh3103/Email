29edc96831ec74b91db25bfab74de90bc1a184a7
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-28 21:32:49 +0530

    [Umesh]Added: checking for first mandatory part

--- src/com/learning/EmailValidation.java
+++ src/com/learning/EmailValidation.java
@@ -1,8 +1,21 @@
 package com.learning;
-
+import java.util.regex.*;
 
 public class EmailValidation {
+	private static final Pattern PIN_CODE = Pattern.compile("^[0-9 a-z A-Z]+");
+    private static boolean checkName(String name) {
+        return PIN_CODE.matcher(name).matches();
+    }
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to email validation problem");
+		
+		boolean flag =checkName("abc");
+		if(flag){
+			System.out.println("Valid email");
+		}
+		else{
+			System.out.println("Invalid email");
+		}
 	}
 }
